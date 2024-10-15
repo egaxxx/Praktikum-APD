1a4688745f0cef4b0e65f4d36e757aa4313fd88b
egaxxx <estahananta@gmail.com>
2024-10-15 20:22:43 +0800

    P

---
+++ Praktikum-APD/kelas b'2/pertemuan ke 3/pertemuan 3.py
@@ -0,0 +1,205 @@
+<<<<<<< HEAD
+#cuaca ="cerah"
+
+#if cuaca =="cerah":
+    print ("kamu pergi keluar rumah")
+#   print("kamu pergi keluar rumah")
+
+    # cuaca ="cerah"
+
+#if cuaca =="cerah":
+#   print("kamu pergi keluar rumah")elif cuaca =="mendung":
+   
+#cuaca ="cerah"
+# else:
+#     print("Invalid")
+
+
+
+
+# cuaca = input ("masukkan cuaca hari ini:")
+# if cuaca == "cerah":
+#    print("kamu keluar rumah")
+# else:
+#     print("kamu tetap di rumah")
+
+# cuaca = input("masukkan cuaca hari ini")
+# if cuaca == "cerah":
+#     print("kamu keluar rumah")
+# elif cuaca == "mendung"
+#      print("baca komik")
+# else:
+#     print("kamu tidur")
+
+# opsi = input("""pilih menu :
+# 1. kondisi 1
+# 2. kondisi 2
+# 3. kondisi 3 """)
+# masukan pilihan menu :
+
+# if opsi == "1":
+#     print("kondisi 1")
+# elif opsi == "2"
+#       print("kondisi 2")
+# else:
+#     print("tidak valid")
+
+
+
+
+# umur = int(input("masukan umur: "))
+# if umur <0:
+#     print("input tidak valid")
+# elif(umur <= 10) :
+#     print("anak-anak")
+# elif(umur <= 18) :
+#     print("remaja")
+# elif(umur <= 60) :
+#     print("dewasa")
+
+# angka = int(input("masukkan angka:"))
+# result = "genap" if angka % 2 == 0 else "ganjil"
+
+# print(angka,"adalah angka",result)
+
+# a = 10
+# b = 20
+# print("a lebih besar dari b") if a > b else print("a lebih kecil dari b")
+
+# if a > b:
+#     print("a lebih besar dari b")
+# else:
+#     print("a lebih kecil dari b")
+
+# string = "abc"
+# int = 0
+# print(bool(string))
+# print(bool(int))
+
+# penggunaan and, or, not
+# a = 10
+# b = 20
+# if a < b and a % 2 == 0:
+#     print("a lebih kecil dari b dan a adalah bilangan genap")
+# elif a > b or a % 2 == 0:
+#     print("a lebih besar dari b dan a adalah bilangan genap")
+# else:
+#     print("a lebih kecil dari b dan a adalah bilangan ganjil")
+
+# print("""pilih menu:
+# # 1. kondisi 1
+# # 2. kondisi 2
+# # 3. kondisi 3""")
+# opsi = input("pilih menu: ")
+# match opsi:
+#     case "1"
+#        print("kondisi 1")
+#     case "2"
+#        print("kondisi 2")
+#     case "3"
+#        print("kondisi 3")
+#     case _:
+#         print("input tidak valid")
+=======
+#cuaca ="cerah"
+
+#if cuaca =="cerah":
+    print ("kamu pergi keluar rumah")
+#   print("kamu pergi keluar rumah")
+
+    # cuaca ="cerah"
+
+#if cuaca =="cerah":
+#   print("kamu pergi keluar rumah")elif cuaca =="mendung":
+   
+#cuaca ="cerah"
+# else:
+#     print("Invalid")
+
+
+
+
+# cuaca = input ("masukkan cuaca hari ini:")
+# if cuaca == "cerah":
+#    print("kamu keluar rumah")
+# else:
+#     print("kamu tetap di rumah")
+
+# cuaca = input("masukkan cuaca hari ini")
+# if cuaca == "cerah":
+#     print("kamu keluar rumah")
+# elif cuaca == "mendung"
+#      print("baca komik")
+# else:
+#     print("kamu tidur")
+
+# opsi = input("""pilih menu :
+# 1. kondisi 1
+# 2. kondisi 2
+# 3. kondisi 3 """)
+# masukan pilihan menu :
+
+# if opsi == "1":
+#     print("kondisi 1")
+# elif opsi == "2"
+#       print("kondisi 2")
+# else:
+#     print("tidak valid")
+
+
+
+
+# umur = int(input("masukan umur: "))
+# if umur <0:
+#     print("input tidak valid")
+# elif(umur <= 10) :
+#     print("anak-anak")
+# elif(umur <= 18) :
+#     print("remaja")
+# elif(umur <= 60) :
+#     print("dewasa")
+
+# angka = int(input("masukkan angka:"))
+# result = "genap" if angka % 2 == 0 else "ganjil"
+
+# print(angka,"adalah angka",result)
+
+# a = 10
+# b = 20
+# print("a lebih besar dari b") if a > b else print("a lebih kecil dari b")
+
+# if a > b:
+#     print("a lebih besar dari b")
+# else:
+#     print("a lebih kecil dari b")
+
+# string = "abc"
+# int = 0
+# print(bool(string))
+# print(bool(int))
+
+# penggunaan and, or, not
+# a = 10
+# b = 20
+# if a < b and a % 2 == 0:
+#     print("a lebih kecil dari b dan a adalah bilangan genap")
+# elif a > b or a % 2 == 0:
+#     print("a lebih besar dari b dan a adalah bilangan genap")
+# else:
+#     print("a lebih kecil dari b dan a adalah bilangan ganjil")
+
+# print("""pilih menu:
+# # 1. kondisi 1
+# # 2. kondisi 2
+# # 3. kondisi 3""")
+# opsi = input("pilih menu: ")
+# match opsi:
+#     case "1"
+#        print("kondisi 1")
+#     case "2"
+#        print("kondisi 2")
+#     case "3"
+#        print("kondisi 3")
+#     case _:
+#         print("input tidak valid")
+>>>>>>> 49226045668a316d892c04a5758fd98db44f7be7
