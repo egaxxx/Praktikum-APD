65d6397c73f42ef79c9f93dcd3f08543d9f70987
egaxxx <estahananta@gmail.com>
2024-10-18 15:06:48 +0800

    p

---
+++ kelas b'2/pertemuan 5/pertemuan ke 5.py
@@ -0,0 +1,208 @@
+<<<<<<< HEAD
+# list
+
+# # list1 = ["a",2,True,[1,2,3]]
+
+# # print(list1)[-1][2])
+
+# # tas = ["buku", 32, True, 3.14, [“IF”, 24]]
+
+# print (tas)
+
+# tas ["buku", 32, True, 3.14, ["IF", 24]]
+
+# for i in tas:
+#     print (i)
+# print(len(tas))
+
+# for i in range(len(tas)):
+#     print(tas[i])
+
+
+
+# matkul = ["kalkulus","fisdas","pti"]
+# print (matkul)
+
+# matkul .append("sastra Mesin")
+# print (matkul)
+# matkul.insert(2,"sastra ")
+
+# matkul[1] = "fisika quantum"
+# print(matkul)
+
+# del matkul [1]
+# print(matkul)
+
+# matkul.pop(1)
+# print(matkul)
+
+# buang = matkul.pop(1)
+# print(f"variabel buang ={buang}")
+
+# prodi = ["Informatika," "Sistem Informasi", "Teknik Arsitektur", "Teknik Lingkungan", "Teknik Pertambangan","Teknik Elektro", "Teknik Industri", "Teknik Sipil", "TeknikGeologi", "Teknik Kimia"]
+# print(prodi[:2], "\n", prodi)
+
+# pyrochar = ["hu tao", "klee", "xianling"]
+# hydrochar = ["furina","nevuliette","lapu lapu"]
+
+# party = pyrochar + hydrochar
+# print(hydrochar)
+# print(pyrochar)
+# print(party)
+
+# print(pyrochar*3)
+
+# barang = [["sepatu", "tas", "baju"],
+#           ["pulpen","pensil","laptop"]]
+# print(barang [1][1])
+
+
+# print(barang [-2][-1])
+
+# for i in barang:
+#     for j in i:
+#         print(j)
+
+# for baris in barang:
+#     for kolom in baris:
+#         print(kolom)
+
+#TUPLE
+
+# nama = ("aldy","daffa","arishy")
+
+# print(nama)
+# nama.append("rava")
+# print(nama)
+
+# barang1 = ("sepatu", "tas", "baju")
+# barang2 = ("pulpen","pensil","laptop")
+
+# barang = (barang1,barang2)
+# print(barang)
+
+
+
+# #mendeklarasikan tuple
+# mahasiswa = (69, "Informatika", "2209106044", "Aldy septian ")
+# #lalu kita unpacking
+# absen, prodi, nim, nama = mahasiswa
+# #maka ketiga variabel tersebut akan berisi data dari tuple mahasiswa
+# #menampilkan variabel
+
+# print(absen)
+# print(prodi)
+# print(nim)
+# print(nama)
+
+
+# mahasiswa = (69, "informatika", "2209106044", "Aldy septian")
+# mahasiswa = list(mahasiswa)
+# mahasiswa[2] = "2409106069"
+
+# mahasiswa = tuple(mahasiswa)
+=======
+# list
+
+# # list1 = ["a",2,True,[1,2,3]]
+
+# # print(list1)[-1][2])
+
+# # tas = ["buku", 32, True, 3.14, [“IF”, 24]]
+
+# print (tas)
+
+# tas ["buku", 32, True, 3.14, ["IF", 24]]
+
+# for i in tas:
+#     print (i)
+# print(len(tas))
+
+# for i in range(len(tas)):
+#     print(tas[i])
+
+
+
+# matkul = ["kalkulus","fisdas","pti"]
+# print (matkul)
+
+# matkul .append("sastra Mesin")
+# print (matkul)
+# matkul.insert(2,"sastra ")
+
+# matkul[1] = "fisika quantum"
+# print(matkul)
+
+# del matkul [1]
+# print(matkul)
+
+# matkul.pop(1)
+# print(matkul)
+
+# buang = matkul.pop(1)
+# print(f"variabel buang ={buang}")
+
+# prodi = ["Informatika," "Sistem Informasi", "Teknik Arsitektur", "Teknik Lingkungan", "Teknik Pertambangan","Teknik Elektro", "Teknik Industri", "Teknik Sipil", "TeknikGeologi", "Teknik Kimia"]
+# print(prodi[:2], "\n", prodi)
+
+# pyrochar = ["hu tao", "klee", "xianling"]
+# hydrochar = ["furina","nevuliette","lapu lapu"]
+
+# party = pyrochar + hydrochar
+# print(hydrochar)
+# print(pyrochar)
+# print(party)
+
+# print(pyrochar*3)
+
+# barang = [["sepatu", "tas", "baju"],
+#           ["pulpen","pensil","laptop"]]
+# print(barang [1][1])
+
+
+# print(barang [-2][-1])
+
+# for i in barang:
+#     for j in i:
+#         print(j)
+
+# for baris in barang:
+#     for kolom in baris:
+#         print(kolom)
+
+#TUPLE
+
+# nama = ("aldy","daffa","arishy")
+
+# print(nama)
+# nama.append("rava")
+# print(nama)
+
+# barang1 = ("sepatu", "tas", "baju")
+# barang2 = ("pulpen","pensil","laptop")
+
+# barang = (barang1,barang2)
+# print(barang)
+
+
+
+# #mendeklarasikan tuple
+# mahasiswa = (69, "Informatika", "2209106044", "Aldy septian ")
+# #lalu kita unpacking
+# absen, prodi, nim, nama = mahasiswa
+# #maka ketiga variabel tersebut akan berisi data dari tuple mahasiswa
+# #menampilkan variabel
+
+# print(absen)
+# print(prodi)
+# print(nim)
+# print(nama)
+
+
+# mahasiswa = (69, "informatika", "2209106044", "Aldy septian")
+# mahasiswa = list(mahasiswa)
+# mahasiswa[2] = "2409106069"
+
+# mahasiswa = tuple(mahasiswa)
+>>>>>>> 49226045668a316d892c04a5758fd98db44f7be7
+# print(mahasiswa)
